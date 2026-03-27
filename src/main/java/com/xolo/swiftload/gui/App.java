e2e2dd215e6301391520854db5b84cd18556d70e
Xolo978 <prithvirajbanik900@gmail.com>
2026-03-27 22:24:16 +0530

    Dynamic thread slider based on available system resources

--- src/main/java/com/xolo/swiftload/gui/App.java
+++ src/main/java/com/xolo/swiftload/gui/App.java
@@ -7,6 +7,7 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,17 +21,23 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("SwiftLoad Downloader");
+        int availableThreads = Math.max(1, Runtime.getRuntime().availableProcessors());
+        int defaultThreads = Math.min(4, availableThreads);
+
         Label urlLabel = new Label("Download URL:");
         TextField urlField = new TextField("https://ash-speed.hetzner.com/100MB.bin");
+        urlField.setMaxWidth(Double.MAX_VALUE);
 
         Label threadLabel = new Label("Threads:");
-        Slider threadSlider = new Slider(1, 16, 4);
+        Slider threadSlider = new Slider(1, availableThreads, defaultThreads);
         threadSlider.setMajorTickUnit(1);
         threadSlider.setMinorTickCount(0);
         threadSlider.setBlockIncrement(1);
         threadSlider.setShowTickMarks(true);
-        threadSlider.setShowTickLabels(true);
+        threadSlider.setShowTickLabels(availableThreads <= 32);
         threadSlider.setSnapToTicks(true);
+        threadSlider.setMaxWidth(Double.MAX_VALUE);
+        threadSlider.setTooltip(new Tooltip("Detected CPU threads: " + availableThreads));
 
         Button startBtn = new Button("Start Download");
         startBtn.setMaxWidth(Double.MAX_VALUE);
@@ -43,10 +50,12 @@ public class App extends Application {
         threadContainer = new VBox(5);
         ScrollPane scrollPane = new ScrollPane(threadContainer);
         scrollPane.setFitToWidth(true);
-        scrollPane.setPrefHeight(200);
+        scrollPane.setPrefHeight(260);
+        scrollPane.setMaxHeight(Double.MAX_VALUE);
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
+        VBox.setVgrow(scrollPane, Priority.ALWAYS);
         layout.getChildren().addAll(
                 urlLabel, urlField,
                 threadLabel, threadSlider,
@@ -54,8 +63,10 @@ public class App extends Application {
                 scrollPane
         );
 
-        Scene scene = new Scene(layout, 400, 350);
+        Scene scene = new Scene(layout, 560, 520);
         primaryStage.setScene(scene);
+        primaryStage.setMinWidth(500);
+        primaryStage.setMinHeight(420);
         primaryStage.show();
 
         startBtn.setOnAction(_ -> {
